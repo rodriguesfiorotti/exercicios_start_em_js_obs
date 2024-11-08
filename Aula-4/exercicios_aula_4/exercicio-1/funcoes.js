476d8cb20cdcefaf790cb3fd97a795f17a3c98b6
João Victor Rodrigues <rodriguesfiorotti@gmail.com>
2024-11-08 07:53:28 -0300

    Exercicio 2: é uma continuação do exercicio 1

--- Aula-4/exercicios_aula_4/exercicio-1/funcoes.js
+++ Aula-4/exercicios_aula_4/exercicio-1/funcoes.js
@@ -2,3 +2,13 @@ export function dobro (x) {
     let dobrado = (x * 2)
     return dobrado
 }
+
+export function somar (x, y) {
+    let somatorio = (x + y)
+    return somatorio
+}
+
+export function subtrair (x, y) {
+    let subtracao = (x - y)
+    return subtracao
+}
--- Aula-4/exercicios_aula_4/exercicio-1/main.js
+++ Aula-4/exercicios_aula_4/exercicio-1/main.js
@@ -1,6 +1,21 @@
-import { dobro } from "./funcoes.js";
+import { dobro, somar, subtrair } from "./funcoes.js";
 
-let numero = parseInt(prompt("Informe um número inteiro!"));
-let resultado = dobro(numero); // Armazena o valor retornado pela função
+let opcao_calculo = parseInt(prompt("Escolha uma opção: \n\nSomar: Digite 1\nSubtrair: Digite 2\n Descobrir o dobro de um número: Digite 3"));
 
-alert(`O dobro de ${numero} é ${resultado}`);
+switch(opcao_calculo) {
+    case 1 :
+        let sum1 = parseInt(prompt("Informe o primeiro número para o somatório"));
+        let sum2 = parseInt(prompt("Informe o segundo número para o somatório"));
+        alert(`${sum1} + ${sum2} = ${somar(sum1, sum2)}`);
+        break;
+    case 2 :
+        let subt1 = parseInt(prompt("Informe o primeiro número para a subtração"));
+        let subt2 = parseInt(prompt("Informe o segundo número para a subtração"));
+        alert(`${subt1} - ${subt2} = ${subtrair(subt1, subt2)}`);
+        break;
+    case 3 :
+        let numero = parseInt(prompt("Informe um número inteiro!"));
+        let resultado = dobro(numero);
+        alert(`O dobro de ${numero} é ${resultado}`);
+        break;
+}
