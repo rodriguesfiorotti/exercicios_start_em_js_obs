a15544fd6a42801ce39c9388837b39521fd03a2c
João Victor Rodrigues <rodriguesfiorotti@gmail.com>
2025-01-06 21:56:54 -0300

    Adicionei campos para progresso e forma de destacar livros concluidos

--- Aula-8/Exercicios/exercicio_2/exercicio2/main.js
+++ Aula-8/Exercicios/exercicio_2/exercicio2/main.js
@@ -1,7 +1,10 @@
+const h2Title = document.getElementById("h2Title")
 const bookForm = document.getElementById("bookForm");
 const titleInput = document.getElementById("title");
 const genrerInput = document.getElementById("genrer");
 const synopsisText = document.getElementById("synopsis");
+const readPagesInput = document.getElementById("readPagesInput")
+const totalPagesInput = document.getElementById("totalPagesInput")
 const addBookBtn = document.getElementById("addBookBtn");
 const bookList = document.getElementById("bookList");
 let isEditing = false;
@@ -17,22 +20,24 @@ bookForm.addEventListener("submit", function (event) {
     const title = titleInput.value;
     const genrer = genrerInput.value;
     const synopsis = synopsisText.value;
+    const readPages = readPagesInput.value;
+    const totalPages = totalPagesInput.value;
 
     if (isEditing) {
-        updateBook(editingBookId, title, genrer, synopsis);
+        updateBook(editingBookId, title, genrer, synopsis, readPages, totalPages);
     } else {
-        addBook(title, genrer, synopsis);
+        addBook(title, genrer, synopsis, readPages, totalPages);
     }
 });
 
-async function addBook(title, genrer, synopsis) {
+async function addBook(title, genrer, synopsis, readPages, totalPages) {
     try {
         const resposta = await fetch("http://localhost:3000/books", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json", // Corrigido
+                "Content-Type": "application/json", 
             },
-            body: JSON.stringify({ title, genrer, synopsis}), // Corrigido
+            body: JSON.stringify({ title, genrer, synopsis, readPages, totalPages }), 
         });
 
         if (!resposta.ok) {
@@ -64,6 +69,8 @@ async function loadBookList() {
                 titleInput.value = book.title;
                 genrerInput.value = book.genrer;
                 synopsisText.value = book.synopsis;
+                readPagesInput.value = book.readPages;
+                totalPagesInput.value = book.totalPages;
 
                 toggleEditMode(true, bookId);
             } else {
@@ -74,12 +81,15 @@ async function loadBookList() {
 }
 
 
-
 function showBooksList(booksInfo) {
     bookList.innerHTML = ""
     booksInfo.forEach(function (books) {
         const listItem = document.createElement("li")
-        listItem.innerHTML = `<article> <h3>${books.title}</h3> <p>${books.genrer} <p>${books.synopsis}</p> <button type="button"  id="${books.id}" class="editButton">Editar</button> </article><hr>`
+        if (books.readPages >= books.totalPages) {
+            listItem.innerHTML = `<article class = "concluido"> <h3>${books.title} - Livro Concluído! </h3> <p>${books.genrer} <p>${books.synopsis}</p> <p>Progresso: ${books.readPages}/${books.totalPages} páginas</p> <button type="button"  id="${books.id}" class="editButton">Editar</button> <button type="button" class="deleteButton">Excluir</button> </article><hr>`
+        } else {
+            listItem.innerHTML = `<article> <h3>${books.title}</h3> <p>${books.genrer} <p>${books.synopsis}</p> <p>Progresso: ${books.readPages}/${books.totalPages} páginas</p> <button type="button"  id="${books.id}" class="editButton">Editar</button> <button type="button" class="deleteButton">Excluir</button> </article><hr>`
+        }
 
         bookList.appendChild(listItem)
     })
@@ -89,6 +99,8 @@ function limparFormulario() {
     titleInput.value = "";
     genrerInput.value = "";
     synopsisText.value = "";
+    readPagesInput.value = "";
+    totalPagesInput.value = "";
     toggleEditMode(false);
 }
 
@@ -99,24 +111,26 @@ function findBookById(booksInfo, id) {
 function toggleEditMode(editing, bookId = null) {
     const submitButton = bookForm.querySelector('button[type="submit"]');
     if (editing) {
+        h2Title.textContent = "Editando Livro";
         submitButton.textContent = "Salvar Alterações";
         isEditing = true;
         editingBookId = bookId;
     } else {
+        h2Title.textContent = "Novo Livro";
         submitButton.textContent = "Adicionar Livro a Lista";
         isEditing = false;
         editingBookId = null;
     }
 }
 
-async function updateBook(id, title, genrer, synopsis) {
+async function updateBook(id, title, genrer, synopsis, readPages, totalPages) {
     try {
         const resposta = await fetch(`http://localhost:3000/books/${id}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ title, genrer, synopsis }),
+            body: JSON.stringify({ title, genrer, synopsis, readPages, totalPages }),
         });
 
         if (!resposta.ok) {
@@ -125,7 +139,28 @@ async function updateBook(id, title, genrer, synopsis) {
 
         console.log(`Livro com ID=${id} atualizado com sucesso.`);
         limparFormulario();
-        toggleEditMode(false); // Retornar ao modo de adição
+        toggleEditMode(false); 
+        loadBookList();
+    } catch (erro) {
+        console.error(erro.message);
+    }
+}
+
+async function deleteBook(id) {
+    try {
+        const resposta = await fetch(`http://localhost:3000/books/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        if (!resposta.ok) {
+            throw new Error(`Erro ao deletar o livro: ${resposta.statusText}`);
+        }
+
+        console.log(`Livro com ID=${id} excluído com sucesso.`);
+        limparFormulario(); 
         loadBookList();
     } catch (erro) {
         console.error(erro.message);
